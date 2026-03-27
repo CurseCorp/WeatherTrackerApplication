0479aa99cf8db73f8dd965c2db836fb952243e39
M1kr03x <countdownlfg@mail.ru>
2026-03-27 17:32:54 +0300

    Add network folder for working with weather api

---
+++ networks/weatherclient.cpp
@@ -0,0 +1,3 @@
+#include "weatherclient.h"
+
+WeatherClient::WeatherClient() {}
---
+++ networks/weatherclient.h
@@ -0,0 +1,13 @@
+#ifndef WEATHERCLIENT_H
+#define WEATHERCLIENT_H
+
+#include <QObject>
+
+class WeatherClient
+{
+    Q_OBJECT
+public:
+    WeatherClient();
+};
+
+#endif // WEATHERCLIENT_H
